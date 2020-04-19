d98e1608289e0de422dad93b92c4744de07d9999
Ash Wilding <46906049+ashwio@users.noreply.github.com>
2020-04-19 13:46:26 +0100

    Allow log functions to be called without arguments
    Will print an prefixed empty line, e.g. log.info() simply prints "[INFO]"
    
    This is useful for inserting blank lines without losing the logging prefix,
    rather than using explicit "\n" characters.

--- pgtt/log.py
+++ pgtt/log.py
@@ -8,16 +8,16 @@ SPDX-License-Identifier: MIT
 from . import args
 
 
-def info( msg:str ) -> None:
-    print(f"[INFO] {msg}")
+def info( msg:str="" ) -> None:
+    print(f"[INFO] {msg if msg else ''}")
 
-def verbose( msg:str ) -> None:
+def verbose( msg:str="" ) -> None:
     if (args.verbose):
-        print(f"[VERBOSE] {msg}")
+        print(f"[VERBOSE] {msg if msg else ''}")
 
-def debug( msg:str ) -> None:
+def debug( msg:str="" ) -> None:
     if (args.debug):
-        print(f"[DEBUG] {msg}")
+        print(f"[DEBUG] {msg if msg else ''}")
 
-def error( msg:str ) -> None:
-    print(f"[ERROR] {msg}")
+def error( msg:str="" ) -> None:
+    print(f"[ERROR] {msg if msg else ''}")
