8e63904a04e09f65eff91310c6f0053e019d80bf
Ash Wilding <46906049+ashwio@users.noreply.github.com>
2020-04-19 19:55:48 +0100

    Write generated assembly to output file

--- pgtt/__init__.py
+++ pgtt/__init__.py
@@ -31,5 +31,13 @@ Generate assembly to program the MMU and translation tables at runtime.
 """
 from . import codegen
 
-
-print(codegen.template)
+"""
+Write generated assembly to output file.
+"""
+try:
+    with open(args.o, "w") as f:
+        f.write(codegen.output)
+except OSError as e:
+    import sys
+    log.error(e)
+    sys.exit(e.errno if hasattr(e, "errno") else 1)
