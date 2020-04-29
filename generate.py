7d2c825bc61625f6463c15683842a9f1ccc0d78d
Ash Wilding <46906049+ashwio@users.noreply.github.com>
2020-04-29 18:04:45 +0100

    Ensure user is running with Python 3.8+

--- generate.py
+++ generate.py
@@ -6,4 +6,8 @@ SPDX-License-Identifier: MIT
 Run the arm64-pgtable-tool.
 """
 
+from sys import version_info
+if version_info < (3, 8):
+    raise Exception("arm64-pgtable-tool requires Python 3.8+")
+
 import pgtt
