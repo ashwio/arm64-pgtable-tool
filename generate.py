c3b6a254edebcd178a1c89f5c084d8d9bcb484a8
Ash Wilding <46906049+ashwio@users.noreply.github.com>
2020-04-29 21:13:16 +0100

    Add user-friendly message for IntervalTree dependency

--- generate.py
+++ generate.py
@@ -8,6 +8,13 @@ Run the arm64-pgtable-tool.
 
 from sys import version_info
 if version_info < (3, 8):
-    raise Exception("arm64-pgtable-tool requires Python 3.8+")
+    print("arm64-pgtable-tool requires Python 3.8+")
+    exit()
+
+try:
+    import IntervalTree
+except ModuleNotFoundError as e:
+    print("arm64-pgtable-tool requires IntervalTree: `pip install intervaltree`")
+    exit()
 
 import pgtt
